5876856de19b01cb3abfcd7018d0f853d6130cd4
kim400im <min2000jae@naver.com>
2025-06-06 15:36:26 +0900

    profile

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,11 @@
 // lib/auth.ts
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
 // import type { User } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// const supabase = createClient(
+//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// );
 
 // export const handleSignIn = async (): Promise<void> => {
 //   const { error } = await supabase.auth.signInWithOAuth({
